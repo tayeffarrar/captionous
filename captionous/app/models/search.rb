f1c3dd02106adc51f8b8f6a98a620a379883a838
Colin O'Rourke <corour01@gmail.com>
2015-03-12 16:48:30 -0400

    search model start

--- captionous/app/models/search.rb
+++ captionous/app/models/search.rb
@@ -1,3 +1,21 @@
 class Search < ActiveRecord::Base
 	has_many :pictures
+
+	before_create: fetch_results
+
+	def fetch_results
+		results = instagram.tag_recent_media(query)
+		results.each do |result|
+			self.picturess << Picture.new {
+				url: result.images.standard_resolution
+			}
+		end
+	end
+
+	private
+
+	def instagram
+		Rails.config.instagram_client
+	end
+	
 end
