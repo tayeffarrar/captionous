f0ba0ab8a3f75c8ee59c80da334666ac5bb9d1ac
Colin O'Rourke <corour01@gmail.com>
2015-03-14 12:23:59 -0400

    christian help

--- captionous/app/assets/javascripts/pictures.js
+++ captionous/app/assets/javascripts/pictures.js
@@ -1,3 +1,35 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
+console.log("everything is ok at pictures.js");
+
+$(function(){
+	$.ajax({
+		url: '/pictures',
+		method: 'GET',
+		data: @testtag,
+		dataType: 'json',
+		success: function(data){
+			data.forEach(function(pictures){
+				renderPicture(picture);
+			})
+		}
+	})
+	$('#search-picture-keyword').on('submit', function(e){
+		e.preventDefault();
+
+		values = {
+			keyword:
+				$('#search-picture-keyword').value
+		}
+
+		$(this).children('input').not('input[type=submit]').val('');
+
+		$.ajax({
+			url: '/pictures',
+			method: 'POST',
+			data: values,
+			dataType: 'json',
+			success: function(picture){
+				renderPicture(picture);
+			}
+		})
+	})
+})
--- captionous/app/controllers/pictures_controller.rb
+++ captionous/app/controllers/pictures_controller.rb
@@ -1,3 +1,25 @@
 class PicturesController < ApplicationController
-	
+
+def show
+	Instagram.configure do |config|
+		config.client_id = Rails.application.secrets.instagram_client_id
+		config.client_secret = Rails.application.secrets.instagram_client_secret
+	end
+
+	# Pry.start(binding)
+	client = Instagram.client 
+	# client.media_popular
+	# client.tag_search('')
+	testtag = client.tag_recent_media('highway')[0].images.standard_resolution.url 
+	# Pry.start(binding)
+
+end	
+
+	# def instagram
+	# 	Instagram.configure do |config|
+	# 		config.client_id = Rails.application.secrets.instagram_client_id
+	# 		config.client_secret = Rails.application.secrets.instagram_client_secret
+	# 	end
+	# end
+
 end
