d2d11b9a372b02921124b6324e07fdc99ad0e583
tayef farrar <design@tayef.me>
2015-03-17 15:23:38 -0400

    fetch wat

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,7 +2,7 @@ class Search < ActiveRecord::Base
 	has_many :pictures
 	has_many :captions
 
-	before_create :fetch_results
+	# before_create :fetch_results
 
 	# def fetch_results
 	# 	results = instagram.tag_recent_media(keyword)
